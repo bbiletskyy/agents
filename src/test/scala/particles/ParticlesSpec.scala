5a62050bff851c964ce695be089b868522121cde
Borys Biletskyy <bbiletskyy@Boryss-MacBook-Pro.local>
2019-06-15 12:19:32 +0200

    unite 2 particle tests into 1

--- src/test/scala/particles/ParticlesSpec.scala
+++ src/test/scala/particles/ParticlesSpec.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import org.scalatest.{FunSpecLike, MustMatchers}
 
 class ParticlesSpec extends FunSpecLike with MustMatchers {
-  
+
   describe("Particle") {
     it("should return energy = 1 when there is only one particle") {
       val p = FloatingParticle('*', 1)
@@ -36,40 +36,6 @@ class ParticlesSpec extends FunSpecLike with MustMatchers {
       newParticle.pos mustBe particle.pos + 1
     }
   }
-  describe("StaticParticle") {
-    it("should return energy = 0 when there is only one particle") {
-      val p = StaticParticle('|', 1)
-      val neighborhood = Map(p.id -> p)
-      p.energy(neighborhood) mustBe 0
-    }
-
-    it("should preserve energy = 0 after transformation") {
-      val p = StaticParticle('|', 1)
-      val neighborhood = Map(p.id -> p)
-      val newNeighborhood = p.transform(neighborhood)
-
-      p.energy(newNeighborhood) mustBe 0
-    }
-
-    it("should not change particle location after transformation") {
-      val particle = StaticParticle('|', 0, UUID.randomUUID().toString, 1L)
-      val neighborhood = Map(particle.id -> particle)
-      val newNeighborhood = particle.transform(neighborhood)
-      val newParticle = newNeighborhood.map{ case (id, particle) => particle}.head
-      newParticle.pos mustBe particle.pos
-    }
-
-    it("should not change neighborhood after transformation") {
-      val particle = StaticParticle('|', 0, UUID.randomUUID().toString, 1L)
-      val neighborhood = Map(particle.id -> particle)
-      val newNeighborhood = particle.transform(neighborhood)
-
-      neighborhood mustBe newNeighborhood
-    }
-  }
-}
-
-class StaticParticleSpec extends FunSpecLike with MustMatchers {
   describe("StaticParticle") {
     it("should return energy = 0 when there is only one particle") {
       val p = StaticParticle('|', 1)
